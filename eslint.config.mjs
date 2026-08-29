d432a70c34395c1446b99441d753c7ba6a24c5cb
christopher Dikesa <chrismsmr@gmail.com>
2026-08-29 09:24:09 +0100

    Add ESLint configuration with TypeScript rules

---
+++ eslint.config.mjs
@@ -0,0 +1,30 @@
+import eslint from "@eslint/js";
+import tseslint from "typescript-eslint";
+
+export default tseslint.config(
+  {
+    ignores: [
+      "**/.next/**",
+      "**/node_modules/**",
+      "**/dist/**",
+      "**/.turbo/**"
+    ]
+  },
+
+  eslint.configs.recommended,
+
+  ...tseslint.configs.recommended,
+
+  {
+    rules: {
+      "@typescript-eslint/no-explicit-any": "error",
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        {
+          "argsIgnorePattern": "^_",
+          "varsIgnorePattern": "^_"
+        }
+      ]
+    }
+  }
+);
